bd776e6661469e1500ab1516b7b002b4bba1ee94
MartEvrd <martin.evrard67@gmail.com>
2023-10-15 19:08:54 +0200

    initializer

---
+++ src/main/java/com/example/tpspringerp/TpSpringErpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpspringerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpspringerp/TpSpringErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpspringerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
